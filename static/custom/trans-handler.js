04fee665256dc0cb644f262c200172c1e3ff68b8
PSzczepanski1996 <p.szczepanski996@gmail.com>
2022-03-09 14:28:45 +0100

    Add initial translations

---
+++ static/custom/trans-handler.js
@@ -0,0 +1,28 @@
+// Translation handler.
+
+const TRANS_OBJECT = {
+    'Polski': [
+        'Nowa gra',
+        'Opcje',
+        'Ranking',
+        'O grze',
+        'Muzyka',
+        'Dzwieki z gry',
+        'Język / Language',
+        'Powrót do menu',
+        'Wróć do gry',
+        'Przejdź do menu',
+    ],
+    'English': [
+        'New game',
+        'Options',
+        'Ranks',
+        'About the game',
+        'Music',
+        'Audio sounds',
+        'Language',
+        'Return to menu',
+        'Return to game',
+        'Go to menu',
+    ],
+}
