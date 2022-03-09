5869f85496aef9e960b4def1be408f2d08ed8a09
PSzczepanski996 <p.szczepanski996@gmail.com>
2022-03-09 19:50:22 +0100

    Add working translation system

--- static/custom/trans-handler.js
+++ static/custom/trans-handler.js
@@ -1,5 +1,12 @@
 // Translation handler.
 
+let firstRun = true;
+
+const LANGUAGES = [
+    'Polski',
+    'English',
+];
+
 const TRANS_OBJECT = {
     'Polski': [
         'Nowa gra',
@@ -12,6 +19,10 @@ const TRANS_OBJECT = {
         'Powrót do menu',
         'Wróć do gry',
         'Przejdź do menu',
+        'UWAGA! Utrata postępu.',
+        'Oaza',
+        'Cel',
+        'Pauza',
     ],
     'English': [
         'New game',
@@ -24,5 +35,40 @@ const TRANS_OBJECT = {
         'Return to menu',
         'Return to game',
         'Go to menu',
+        'WARNING! Loss of progress.',
+        'Oasis',
+        'Target',
+        'Pause',
     ],
 }
+
+const switchLang = (lang) => {
+    const textElements = [
+        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7',
+        'a', 'span', 'div',
+    ]
+    let currLang = Cookies.get('lang');
+    if(firstRun){
+        currLang = 'Polski';
+        firstRun = false;
+    }
+    $(textElements.join(', ')).each((index, item) => {
+        let getItem = $(item);
+        let getIndex = TRANS_OBJECT[currLang].indexOf(getItem.text());
+        if(getIndex !== -1 && !getItem.children().length){
+            getItem.text(TRANS_OBJECT[lang][getIndex]);
+        }
+    })
+    Cookies.set('lang', lang);
+}
+
+$(document).ready((e) => {
+    let lang = Cookies.get('lang');
+    if(!lang){
+        lang = 'Polski';
+        Cookies.set('lang', lang)
+    }
+    if(lang !== 'Polski') switchLang(lang);
+    let getLangElement = $('.lang');
+    if(getLangElement.length) getLangElement.text(lang);
+})
