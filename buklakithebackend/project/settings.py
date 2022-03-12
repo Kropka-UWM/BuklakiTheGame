400ac070157a5d8ec75719c7bd28eb382be92ee8
PSzczepanski996 <p.szczepanski996@gmail.com>
2022-03-12 22:31:42 +0100

    Hm?

--- buklakithebackend/project/settings.py
+++ buklakithebackend/project/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/buklakithebackend/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'apps_static/')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/buklakithebackend/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'public/uploads/')
 
 CKEDITOR_UPLOAD_PATH = 'public/uploads/'
